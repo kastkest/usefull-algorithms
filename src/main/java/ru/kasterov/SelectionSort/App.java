042b4232ffc1d4762d643b3eef5af100a0b75bf3
kastkest <endimion-xi@yandex.ru>
2022-01-08 17:34:38 +0300

    Create algorithm of Selection Sort

---
+++ src/main/java/ru/kasterov/SelectionSort/App.java
@@ -0,0 +1,29 @@
+package ru.kasterov.SelectionSort;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+        int[] array = {2, 15, -1, 4, 100, 54};
+        App app = new App();
+        app.selectionSort(array);
+    }
+
+    public void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = min;
+            array[minId] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
+
